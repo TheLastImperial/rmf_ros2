94332708354a7ce0de8b5886ff30cd07c80815b9
TheLastImperial <TheLastImperial@TheLastImperial.com>
2022-12-29 13:40:16 -0700

    Version to compile with fleet_adapter_tb4.

--- rmf_fleet_adapter/include/rmf_fleet_adapter/agv/FleetUpdateHandle.hpp
+++ rmf_fleet_adapter/include/rmf_fleet_adapter/agv/FleetUpdateHandle.hpp
@@ -380,8 +380,6 @@ private:
 using FleetUpdateHandlePtr = std::shared_ptr<FleetUpdateHandle>;
 using ConstFleetUpdateHandlePtr = std::shared_ptr<const FleetUpdateHandle>;
 
-FleetUpdateHandle::ConsiderRequest consider_all();
-
 } // namespace agv
 } // namespace rmf_fleet_adapter
 
